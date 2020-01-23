4be9fce2416f048a70d800829640432093e552c1
L3n1ad <danieltoth0910@gmail.com>
2020-01-23 19:16:12 +0000

    add test for subtract minus number and combined opearation test

--- Unit and Integration Tasks B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit and Integration Tasks B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -22,6 +22,12 @@ describe('calculator', function () {
     assert.equal(calculator.runningTotal, -2);
   })
 
+  it('it can subtract minus number', function(){
+    calculator.previousTotal = 2;
+    calculator.subtract(-2);
+    assert.equal(calculator.runningTotal, 4);
+  })
+
   it('it can multiply', function(){
     calculator.previousTotal = 2;
     calculator.multiply(2);
@@ -62,6 +68,20 @@ describe('calculator', function () {
   it('it can pick = operator', function(){
     calculator.operatorClick('=');
     assert.equal(calculator.previousOperator, null);
+  })
+
+  it('it can use multiple operators combined with pick number function', function(){
+    calculator.numberClick(5);
+    calculator.operatorClick("+");
+    calculator.numberClick(5);
+    calculator.operatorClick("-");
+    calculator.numberClick(1);
+    calculator.operatorClick("/");
+    calculator.numberClick(3);
+    calculator.operatorClick("*");
+    calculator.numberClick(5);
+    calculator.operatorClick("=");
+    assert.equal(calculator.runningTotal, 15);
   })
 
   it('it can clear previous calculation if runningTotal 0', function(){
