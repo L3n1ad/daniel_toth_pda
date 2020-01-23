8d5ae70d63d5d7a0869469e576a78af481cc9ed1
L3n1ad <danieltoth0910@gmail.com>
2020-01-23 19:45:06 +0000

    tested for negative, decimal and very large numbers

--- Unit and Integration Tasks B/js_calculator_start_point/tests/integration/tests.js
+++ Unit and Integration Tasks B/js_calculator_start_point/tests/integration/tests.js
@@ -132,4 +132,42 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('15');
   })
 
+  it('should work with negative numbers', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number3')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-2');
+  })
+
+  it('should work with decimal numbers', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2.5');
+  })
+
+  it('should work with very large numbers', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('92962962117');
+  })
+
 });
